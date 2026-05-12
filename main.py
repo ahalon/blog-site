8dd85a55f45b0c148758ce233a5a8c37335198dc
ahalon <adam.jan.halon@gmail.com>
2026-05-12 17:49:40 +0200

    feat: fastapi backend

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from fastapi import FastAPI
 from database import models
 from database.database import engine
 from routers import post
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
+# Create the FastAPI app
 app = FastAPI()
 app.include_router(post.router)
 
+# CORS configuration
+origins = [
+    'http://localhost:3000',
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Create the database tables
 models.Base.metadata.create_all(bind=engine)
 
+# Mount the images directory to serve uploaded images
+app.mount("/images", StaticFiles(directory="images"), name="images")
--- routers/post.py
+++ routers/post.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, APIRouter, Depends
+import shutil
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, File
 from database.database import get_db
 from routers.schemas import PostBase
 from sqlalchemy.orm.session import Session
 from database import db_post
+import string
+import random
 
 router = APIRouter(
     prefix="/posts",
@@ -16,3 +19,20 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get('')
 def posts(db: Session = Depends(get_db)):
     return db_post.get_all(db)
+
+@router.delete('/{id}')
+def delete(id: int, db: Session = Depends(get_db)):
+    return db_post.delete(id, db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    # Implementation for uploading image
+    letter = string.ascii_letters
+    random_string = ''.join(random.choice(letter) for i in range(10))
+    new = f'_{random_string}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, 'wb') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return {'filepath': path}
